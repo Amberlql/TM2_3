abc0265a3105f06e0806ae0edbf5d811a1800c15
Amber Liqui Lung <amberliquilung@live.nl>
2024-06-17 09:52:24 +0200

    updated main file

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Welcome"""
+#Import packages
+import numpy as np
+import trimesh
+import matplotlib.pyplot as plt
+
+#Load data
+"Here you can load all your mesh objects and provide them with a fitting name"
+tumor = trimesh.load('')
+SMA = trimesh.load('')
