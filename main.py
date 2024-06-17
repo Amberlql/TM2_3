1a034d7d72d6fb5b936dd9e5dc949df10cc2dbfa
Amber Liqui Lung <amberliquilung@live.nl>
2024-06-17 17:03:57 +0200

    Define intersections for all planes and mesh-objects and visualize the planes

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import trimesh
 import matplotlib.pyplot as plt
 
 #Import modules
-from scripts.visualization import ObjectPlotter
+from scripts.visualization import ObjectPlotter, create_plane_mesh
 from scripts.centerlinepoints import centerline_straightcylinder
+from scripts.intersections import intersection_points, intersection_plane_with_objects
 
 #Load data
 "Here you can load all your mesh objects and provide them with a fitting name"
 tumor = trimesh.load('models/tumor.STL')
 sma = trimesh.load('models/SMA.STL')
+object_meshes = {"tumor":tumor, "SMA":sma}
 
 #Visualize case
 object_plotter = ObjectPlotter()
@@ -22,5 +24,13 @@ object_plotter.add_object(tumor, "y")
 centerline_points, normal_points = centerline_straightcylinder()
 object_plotter.add_points(centerline_points, "m")
 
+#Compute intersection points with planes in the direction of the centerline of a specific vessel
+intersections = intersection_points(object_meshes, centerline_points, normal_points)
+
+#visualize planes
+for center, normal in zip(centerline_points, normal_points):
+    mesh = create_plane_mesh(center, normal, plane_size=8.5)
+    object_plotter.add_object(mesh, "b")
+    
 #Show plot
 object_plotter.show()
--- scripts/intersections.py
+++ scripts/intersections.py
@@ -1,19 +1,36 @@
 import trimesh
 
-def intersection_plane_with_mesh(mesh, plane_normal, plane_origin):
-    isect = trimesh.intersections.mesh_plane(
+def intersection_plane_with_object(mesh, plane_origin, plane_normal):
+    """ Create an intersection plane perpendicular on the plane_normal with plane_origin. 
+    Return intersection points of this plane with the mesh-object"""
+    intersection = trimesh.intersections.mesh_plane(
         mesh,
-        plane_normal=plane_normal,
         plane_origin=plane_origin,
+        plane_normal=plane_normal,
         return_faces=False
     )
-    return isect
+    return intersection
 
 
-def intersection_plane_with_meshes(meshes, plane_normal, plane_origin):
-    intersections_points = []
+def intersection_plane_with_objects(meshes, plane_origin, plane_normal):
+    """ Create an intersection plane perpendicular on the plane_normal with plane_origin. 
+    Return intersection points of this plane with multiple mesh-objects"""
+    intersections_points = {}
     for mesh in meshes:
-        isect = intersection_plane_with_mesh(mesh, plane_normal, plane_origin)
-        intersections_points.append(isect)
+        intersection = intersection_plane_with_object(meshes[mesh], plane_origin, plane_normal)
+        intersections_points[f"{mesh}"] = intersection
     
     return intersections_points
+
+def intersection_points(meshes, plane_origins, plane_normals):
+    """ Create an intersection plane perpendicular on the plane_normal with plane_origin. 
+    Return a dictionary with for every plane the intersection points of this plane with multiple mesh-objects
+    """
+    intersection_points_per_object_per_plane = {}
+    i=0
+    for origin, normal in zip(plane_origins, plane_normals):
+        intersection = intersection_plane_with_objects(meshes, origin, normal)
+        intersection_points_per_object_per_plane[f"plane {i}"] = intersection
+        i+=1
+            
+    return intersection_points_per_object_per_plane
