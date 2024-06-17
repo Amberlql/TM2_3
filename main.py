2420d6df7d2cd2afa646b63bd81e19eacfcdd053
Amber Liqui Lung <amberliquilung@live.nl>
2024-06-17 11:48:40 +0200

    Add extraction of senterline points to main file

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ import numpy as np
 import trimesh
 import matplotlib.pyplot as plt
 
+#Import modules
+from scripts.defineplanes import centerline
+
 #Load data
 "Here you can load all your mesh objects and provide them with a fitting name"
-tumor = trimesh.load('')
-SMA = trimesh.load('')
+tumor = trimesh.load('models/tumor.STL')
+sma = trimesh.load('models/SMA.STL')
+
+#Define constant
+resolution = 0.1 
+
+# Compute the centerline 
+centerline_points = centerline(sma, resolution)
