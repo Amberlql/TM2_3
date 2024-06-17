a5390d15bc951ea95f0d0593b3cb197affb630ed
Amber Liqui Lung <amberliquilung@live.nl>
2024-06-17 14:16:20 +0200

    added centerline points function for a straight cylinder

--- scripts/centerlinepoints.py
+++ scripts/centerlinepoints.py
@@ -1,22 +1,29 @@
 import numpy as np
 from scipy.interpolate import splprep, splev
 
+def normal_straightcylinder(p1, p2):
+    # Calculate the vector from p1 to p2
+    vec = p2 - p1
+    
+    # Calculate the magnitude of the vector
+    mag = np.linalg.norm(vec)
+    
+    # Check for zero magnitude (to avoid division by zero)
+    if mag == 0:
+        return np.zeros_like(vec)  # Return zero vector if points are identical
+    
+    # Normalize the vector
+    cylinder_normal = vec / mag
+    
+    return cylinder_normal
 
-def centerline(vessel, resolution):
-    # Sample points on the surface of the mesh
-    sampled_points = vessel.sample(1000)
+def centerline_straightcylinder():
+    n = 21 #number of points selected on centerline
+    p1 = np.array([0, -15, 0]) #numbers known from mock data creation
+    p2 = np.array([0, 15, 0]) #numbers known from mock data creation
     
-    # Fit a spline to the sampled points to get a smooth curve
-    tck, u = splprep(sampled_points.T, s=0)
-    
-    # Define the resolution for the spline sampling
-    u_fine = np.arange(0, 1, resolution / np.max(u))
-    
-    # Get the smoothed points along the spline
-    smoothed_points = np.array(splev(u_fine, tck)).T
-    
-    # Approximate the centerline as the smoothed spline of the sampled points
-    centerline_points = smoothed_points
-    
-    return centerline_points
-
+    #interpolate 21 points between p1 and p2 to create points on the centerline
+    centerline_points = np.stack([np.linspace(i, j, n) for i,j in zip(p1, p2)], axis=1) 
+    cylinder_normal = normal_straightcylinder(p1, p2)
+    normal_points = np.full(centerline_points.shape, cylinder_normal)
+    return centerline_points, normal_points
