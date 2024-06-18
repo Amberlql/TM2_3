eba9eeb0d0cb0fc2dae123144f950aa6d4f36925
levijn <levijndejager@gmail.com>
2024-06-18 12:30:56 +0200

    added functionality to add points to contour plot

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -80,6 +80,11 @@ class ContourPlotter:
         print(contour.xy)
         self.ax.plot(contour.xy[0], contour.xy[1], color)
         
+    
+    def add_point(self, point, color):
+        """Add point to plot"""
+        self.ax.plot(point.x, point[1].y, color)
+        
     def show(self):
         """Show plot"""
         plt.show()
