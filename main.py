05bad2ad1c74acbc11f586a89936baf74b2d73cb
Amber Liqui Lung <amberliquilung@live.nl>
2024-06-18 15:12:17 +0200

    update main according to new functions

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import trimesh
 import matplotlib.pyplot as plt
 
 #Import modules
-from scripts.visualization import ObjectPlotter, create_plane_mesh
+from scripts.visualization import ObjectPlotter, create_plane_mesh, ContourPlotter
 from scripts.centerlinepoints import centerline_straightcylinder
 from scripts.plane_intersections import intersection_planes_with_objects
 from scripts.contour_creation import create_contour_from_intersection_points
-from scripts.line_intersections import filter_planes
+from scripts.line_intersections import filter_planes, line_intersections
+
+#Initialize global parameters
+per_degree = 3 #Degree of line segments
+example_plane = 12 #Provide as integer
 
 #Load data and add to the dictionary object_meshes
 #Make sure you always name the tumor "tumor..."
@@ -24,24 +28,39 @@ object_plotter = ObjectPlotter()
 object_plotter.add_object(sma, "r")
 object_plotter.add_object(tumor, "y")
 
-#Compute the centerline of the straight cylinder
+#NOTE: ONLY FOR EXAPLE DATA: Compute the centerline of the straight cylinder
 centerline_points, normal_points = centerline_straightcylinder()
 object_plotter.add_points(centerline_points, "m")
 
 #Compute intersection points with planes perpendicular to the direction of the centerline of a specific vessel
 intersections_with_planes = intersection_planes_with_objects(object_meshes, centerline_points, normal_points)
 
-#Visualize planes
-for center, normal in zip(centerline_points, normal_points):
-    mesh = create_plane_mesh(center, normal, plane_size=8.5)
-    object_plotter.add_object(mesh, "b")
-    
-# #Show plot
-# object_plotter.show()
+#Visualize the example plane as mesh
+mesh = create_plane_mesh(centerline_points[example_plane], normal_points[example_plane], plane_size=8.5)
+object_plotter.add_object(mesh, "b")
+object_plotter.show()
 
 #Create contour per object mesh per plane from intersection points
 all_contours = create_contour_from_intersection_points(intersections_with_planes, object_meshes)
 
 #Filter contours to only achieve the planes in which the tumor is present
 all_contours_filtered = filter_planes(all_contours)
-print(all_contours_filtered)
+
+#Compute lines and intersection points for every line for every object in every plane
+all_intersections, lines = line_intersections(all_contours_filtered, per_degree)
+
+#Visualize intersection points for every line for every object a plane as example
+contourplotter = ContourPlotter()
+plane_intersection = all_intersections[f'plane{example_plane}']
+plane_contour = all_contours_filtered[f'plane{example_plane}']
+for object_mesh in plane_contour:
+    contourplotter.add_contour(plane_contour[object_mesh], "r")
+for line in plane_intersection:
+    for mesh in plane_intersection[line]:
+        contourplotter.add_point(plane_intersection[line][mesh], "b")
+        
+#Vizualize the lines of 90, 180, 270 degrees
+contourplotter.add_contour(lines[30], 'black')
+contourplotter.add_contour(lines[60], 'black')
+contourplotter.add_contour(lines[90], 'black')
+contourplotter.show()
